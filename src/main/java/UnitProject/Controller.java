79fa7fee03237027ed0e607090bf1abb7347e069
Abu1804 <AbubakarAli1804@gmail.com>
2023-10-14 16:45:03 +0530

    myweb

---
+++ src/main/java/UnitProject/Controller.java
@@ -0,0 +1,12 @@
+package UnitProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+@GetMapping(value="/get")
+public String get() {
+	return "HelloWorld";
+}
+}
---
+++ src/main/java/UnitProject/MyWebApps1Application.java
@@ -0,0 +1,13 @@
+package UnitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebApps1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebApps1Application.class, args);
+	}
+
+}
